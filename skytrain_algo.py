de25f4d9e8cb5acead098df21b4d976a4368fd18
Edmond Chen Si Jie <edmond.chen@changiairport.com>
2022-11-11 11:26:32 +0800

    Completed working UI draft

--- skytrain_algo.py
+++ skytrain_algo.py
@@ -39,57 +39,42 @@ from strong_sort.strong_sort import StrongSORT
 
 
 VID_FORMATS = 'asf', 'avi', 'gif', 'm4v', 'mkv', 'mov', 'mp4', 'mpeg', 'mpg', 'ts', 'wmv'  # include video suffixes
+CLASSES = [24, 26, 28]
 
 class Algo:
     ''' Class for unattended bag detection algorithm '''
     def __init__(self, **kwargs):
         ''' Initialise global variables and load weights '''
-        self.yolo_weights = kwargs.get("yolo_weights", "yolov7.pt")
+        self.yolo_weights = kwargs.get("yolo_weights", "yolov7.pt") # WEIGHTS / 
         self.yolo_conf = kwargs.get("yolo_conf", 0.05)
         self.strong_sort_weights = kwargs.get("strong_sort_weights", "")
-        
+        self.config_strongsort = 'strong_sort/configs/strong_sort.yaml'
+        self.iou_thres = 0.3 # NMS IOU threshold
+        self.agnostic_nms=False,  # class-agnostic NMS
+        self.device = '' # cuda device, i.e. 0 or 0,1,2,3 or cpu
+        self.half = False # whether to use FP16 half-precision inference
         self._initialise_yolo()
         self._initialise_strongsort()
 
-        self.colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
-        self.outputs = [None] 
-
     def _initialise_yolo(self):
         ''' Make YOLO model and load weights '''
-        # Directories
-        if not isinstance(yolo_weights, list):  # single yolo model
-            exp_name = yolo_weights.stem
-        elif type(yolo_weights) is list and len(yolo_weights) == 1:  # single models after --yolo_weights
-            exp_name = Path(yolo_weights[0]).stem
-            yolo_weights = Path(yolo_weights[0])
-        else:  # multiple models after --yolo_weights
-            exp_name = 'ensemble'
-        exp_name = name if name else exp_name + "_" + strong_sort_weights.stem
-        save_dir = increment_path(Path(project) / exp_name, exist_ok=exist_ok)  # increment run
-        save_dir = Path(save_dir)
-        (save_dir / 'tracks' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
-
-        # Load model
-        device = select_device(device)
-        
-        WEIGHTS.mkdir(parents=True, exist_ok=True)
-        self.model = attempt_load(Path(yolo_weights), map_location=device)  # load FP32 model
-        names, = self.model.names,
-        stride = self.model.stride.max()  # model stride
-        imgsz = check_img_size(imgsz[0], s=stride.cpu().numpy())  # check image size
+        self.device = select_device(self.device)      
+        self.model = attempt_load(Path(self.yolo_weights), map_location=self.device)  # load FP32 model        
+        self.COLORS = [[random.randint(0, 255) for _ in range(3)] for _ in self.model.names]
     
     def _initialise_strongsort(self):
         ''' Make StrongSORT model '''
         # initialize StrongSORT
         cfg = get_config()
-        cfg.merge_from_file(opt.config_strongsort)
+        print("strong sort config file: ", self.config_strongsort)
+        cfg.merge_from_file(self.config_strongsort)
         print(f"\nStrongSORT Configs: {cfg}")
 
         # Create as many strong sort instances as there are video sources
         self.strongsort = StrongSORT(
-                    strong_sort_weights,
-                    device,
-                    half,
+                    self.strong_sort_weights,
+                    self.device,
+                    self.half,
                     max_dist=cfg.STRONGSORT.MAX_DIST,
                     max_iou_distance=cfg.STRONGSORT.MAX_IOU_DISTANCE,
                     max_age=cfg.STRONGSORT.MAX_AGE,
@@ -101,7 +86,8 @@ class Algo:
                 )
         self.strongsort.model.warmup()
         
-    def run(self, frame_idx, path, im, im0s, vid_cap):
+    @torch.no_grad()
+    def run(self, im, im0):
         ''' Run tracking 
         
         Given an image frame, run algorithm to detect unattended bags.
@@ -113,152 +99,49 @@ class Algo:
             detections: LIST of DICTS
                 [ {"bag_id": INT,
                     "bbox": LIST,
-                    "conf": FLOAT} ]'''
+                    "conf": FLOAT} ]
+        '''
         s = ''
         t1 = time_synchronized()
-        im = torch.from_numpy(im).to(device)
-        im = im.half() if half else im.float()  # uint8 to fp16/32
+        im = torch.from_numpy(im).to(self.device)
+        im = im.half() if self.half else im.float()  # uint8 to fp16/32
         im /= 255.0  # 0 - 255 to 0.0 - 1.0
         if len(im.shape) == 3:
             im = im[None]  # expand for batch dim
         t2 = time_synchronized()
 
         # Inference
-        pred = model(im)
+        pred = self.model(im)
         t3 = time_synchronized()
 
         # Apply NMS
-        pred = non_max_suppression(pred[0], conf_thres, iou_thres, classes, agnostic_nms)
+        pred = non_max_suppression(pred[0], self.yolo_conf, self.iou_thres, CLASSES, self.agnostic_nms)
+        det = pred[0]
         
-        # Process detections
-        for i, det in enumerate(pred):  # detections per image
-            seen += 1
-            if webcam:  # nr_sources >= 1
-                p, im0, _ = path[i], im0s[i].copy(), dataset.count
-                p = Path(p)  # to Path
-                s += f'{i}: '
-                txt_file_name = p.name
-                save_path = str(save_dir / p.name)  # im.jpg, vid.mp4, ...
-            else:
-                p, im0, _ = path, im0s.copy(), getattr(dataset, 'frame', 0)
-                p = Path(p)  # to Path
-                # video file
-                if source.endswith(VID_FORMATS):
-                    txt_file_name = p.stem
-                    save_path = str(save_dir / p.name)  # im.jpg, vid.mp4, ...
-                # folder with imgs
-                else:
-                    txt_file_name = p.parent.name  # get folder name containing current img
-                    save_path = str(save_dir / p.parent.name)  # im.jpg, vid.mp4, ...
+        # Pass YOLO detections to StrongSORT
+        if det is not None and len(det):
+            # Rescale boxes from img_size to im0 size
+            det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
 
-            txt_path = str(save_dir / 'tracks' / txt_file_name)  # im.txt
-            s += '%gx%g ' % im.shape[2:]  # print string
-            imc = im0.copy() if save_crop else im0  # for save_crop
+            # Print results
+            for c in det[:, -1].unique():
+                n = (det[:, -1] == c).sum()  # detections per class
+                s += f"{n} {self.model.names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
-            if det is not None and len(det):
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
+            xywhs = xyxy2xywh(det[:, 0:4])
+            confs = det[:, 4]
+            clss = det[:, 5]
 
-                # Print results
-                for c in det[:, -1].unique():
-                    n = (det[:, -1] == c).sum()  # detections per class
-                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+            # pass detections to strongsort
+            t4 = time_synchronized()
+            detections = self.strongsort.update(xywhs.cpu(), confs.cpu(), clss.cpu(), im0)
+            t5 = time_synchronized()
 
-                xywhs = xyxy2xywh(det[:, 0:4])
-                confs = det[:, 4]
-                clss = det[:, 5]
+            print(f'{s}Done. YOLO:({t3 - t2:.3f}s), StrongSORT:({t5 - t4:.3f}s)')
+            return detections, confs
 
-                # pass detections to strongsort
-                t4 = time_synchronized()
-                detections = strongsort.update(xywhs.cpu(), confs.cpu(), clss.cpu(), im0)
-                t5 = time_synchronized()
-
-                print(f'{s}Done. YOLO:({t3 - t2:.3f}s), StrongSORT:({t5 - t4:.3f}s)')
-                return detections
-
-            else:
-                strongsort.increment_ages()
-                print('No detections')
-                detections = []
-            return detections 
-            
-@torch.no_grad()
-def run(
-        source='0',
-        yolo_weights=WEIGHTS / 'yolov5m.pt',  # model.pt path(s),
-        strong_sort_weights=WEIGHTS / 'osnet_x0_25_msmt17.pt',  # model.pt path,
-        config_strongsort=ROOT / 'strong_sort/configs/strong_sort.yaml',
-        imgsz=(640, 640),  # inference size (height, width)
-        conf_thres=0.25,  # confidence threshold
-        iou_thres=0.45,  # NMS IOU threshold
-        max_det=1000,  # maximum detections per image
-        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-        show_vid=False,  # show results
-        save_txt=False,  # save results to *.txt
-        save_conf=False,  # save confidences in --save-txt labels
-        save_crop=False,  # save cropped prediction boxes
-        save_vid=False,  # save confidences in --save-txt labels
-        nosave=False,  # do not save images/videos
-        classes=None,  # filter by class: --class 0, or --class 0 2 3
-        agnostic_nms=False,  # class-agnostic NMS
-        augment=False,  # augmented inference
-        visualize=False,  # visualize features
-        update=False,  # update all models
-        project=ROOT / 'runs/track',  # save results to project/name
-        name='exp',  # save results to project/name
-        exist_ok=False,  # existing project/name ok, do not increment
-        line_thickness=3,  # bounding box thickness (pixels)
-        hide_labels=False,  # hide labels
-        hide_conf=False,  # hide confidences
-        hide_class=False,  # hide IDs
-        half=False,  # use FP16 half-precision inference
-        dnn=False,  # use OpenCV DNN for ONNX inference
-):
-
-
-def parse_opt():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--yolo-weights', nargs='+', type=str, default=WEIGHTS / 'yolov7.pt', help='model.pt path(s)')
-    parser.add_argument('--strong-sort-weights', type=str, default=WEIGHTS / 'osnet_x0_25_msmt17.pt')
-    parser.add_argument('--config-strongsort', type=str, default='strong_sort/configs/strong_sort.yaml')
-    parser.add_argument('--source', type=str, default='0', help='file/dir/URL/glob, 0 for webcam')  
-    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
-    parser.add_argument('--conf-thres', type=float, default=0.5, help='confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.5, help='NMS IoU threshold')
-    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--show-vid', action='store_true', help='display tracking video results')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--save-crop', action='store_true', help='save cropped prediction boxes')
-    parser.add_argument('--save-vid', action='store_true', help='save video tracking results')
-    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
-    # class 0 is person, 1 is bycicle, 2 is car... 79 is oven
-    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --classes 0, or --classes 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--visualize', action='store_true', help='visualize features')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default=ROOT / 'runs/track', help='save results to project/name')
-    parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--line-thickness', default=3, type=int, help='bounding box thickness (pixels)')
-    parser.add_argument('--hide-labels', default=False, action='store_true', help='hide labels')
-    parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
-    parser.add_argument('--hide-class', default=False, action='store_true', help='hide IDs')
-    parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
-    parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
-    opt = parser.parse_args()
-    opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
-
-    return opt
-
-
-def main(opt):
-    check_requirements(requirements=ROOT / 'requirements.txt', exclude=('tensorboard', 'thop'))
-    run(**vars(opt))
-
-
-if __name__ == "__main__":
-    opt = parse_opt()
-    main(opt)
+        else:
+            self.strongsort.increment_ages()
+            print('No detections')
+            detections = []
+        return detections, []
